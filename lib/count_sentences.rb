fdbe6a57fc9c76b223e1566d4cb82e09b7d6680d
Joe <hutcheson2001@yahoo.com>
2022-04-14 14:29:34 -0400

    commit tree 610767271a218697dceb526e37df6c27aef1aba0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[.!?]/)
+    final_arr = arr.filter {|str| str!=""}
+    final_arr.length
   end
 end
